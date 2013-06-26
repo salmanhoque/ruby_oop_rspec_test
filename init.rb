691cd854c179d35f01e402b0a1276a6f97ea7f86
Salman Hoque <s.ulhoque@gmail.com>
2013-06-26 18:30:47 +1000

    Few test files added to create different test for each class

---
+++ init.rb
@@ -0,0 +1,5 @@
+#TO get the current directory
+APP_ROOT = File.dirname(__FILE__)
+
+#This instruction searches class files in the given folder
+$:.unshift(File.join(APP_ROOT, 'lib'))
---
+++ rspec/test_movie.rb
@@ -0,0 +1,10 @@
+require File.expand_path('lib/movie.rb')
+
+describe "#MovieClass" do 
+
+	it "should have a movie class" do 
+		a = Movie.new()
+	end
+
+	
+end
