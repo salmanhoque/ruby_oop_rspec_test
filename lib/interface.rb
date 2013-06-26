626e6cda6acdfbb99a39f3fbe01ce82ae6c37072
Salman Hoque <s.ulhoque@gmail.com>
2013-06-27 01:40:59 +1000

    Added few tests and some user method into interface

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,10 +1,52 @@
 class Interface
 
+	def initialize(path=nil)
+		
+	end
+
 	def launch!
 		welcome_msg
+		loop  unless action == :quit
+			action = user_action
+		end			
+		goodbye_msg
 	end
-	
+
+	def user_action
+		print "> " 
+		user_action = gets.chomp.strip	
+		return user_action
+	end
+
+	def actions(action=nil)
+		case action
+		when 'add'
+			add_movie
+		when 'list'
+			list	
+		when 'find'
+			find	
+		end
+	end
+
+	def add_movie
+		puts "adding..."
+	end
+
+	def list
+		puts "linting..."
+	end
+
+	def find
+		puts "finding..."
+	end
+
 	def welcome_msg
-		puts "<<< WELCOME TO THE MOVIE APP >>>"		
+		puts "\n\n<<< WELCOME TO THE MOVIE APP >>>\n\n"		
 	end
+
+	def goodbye_msg
+		puts "\n\n<<< GoodBye! >>>\n\n"
+	end
+
 end
--- rspec/test_interface.rb
+++ rspec/test_interface.rb
@@ -3,7 +3,7 @@ require File.expand_path('lib/interface.rb')
 describe "InterfaceClass"  do 
 
    	before :each do
-   	 	@a = Interface.new 
+   	 	@a = Interface.new("movie.txt") 
    	end
 
    	context "should have a method to launch the app" do
@@ -14,11 +14,41 @@ describe "InterfaceClass"  do
    	context "launch method should call welcome_msg" do
    		it { 
    			obj = double()
-			obj.stub(:msg => "Welcome" )
-			obj.stub(:msg).with("Welcome")
+			obj.stub(:msg => "\n\n<<< WELCOME TO THE MOVIE APP >>>\n\n" )
+			obj.stub(:msg).with(@a.welcome_msg)
    		}
    	end
    	
-   	
+   	context "should have a goodbye method" do
+   		it { 
+   			obj = double
+   			obj.stub(:gb_msg => "\n\n<<< GoodBye! >>>\n\n")
+   			obj.stub(:gb_msg).with(@a.goodbye_msg)
+   		 }
+   	end
+
+   	context "if user input is add" do
+   		it { 
+   			obj = double
+   			obj.stub(:add => "adding...")
+   			obj.stub(:add).with(@a.actions('add'))
+   		}
+    end
+
+    context "if user input is listing" do
+   		it { 
+   			obj = double
+   			obj.stub(:list => "listing...")
+   			a = obj.stub(:list).with(@a.actions('list'))
+   		}
+    end
+
+    context "if user input is finding" do
+   		it { 
+   			obj = double
+   			obj.stub(:find => "finding...")
+   			a = obj.stub(:find).with(@a.actions('find'))
+   		}
+    end
 
 end
